2a3002a2e0a76ae822bda1cca5aaf0cee4e8269c
muhammadqasimhunain <muhammad.qasim1255@hotmail.com>
2017-04-18 22:23:12 +0500

    Initial Running

---
+++ build/Debug/GNU-Linux/main.o.d
@@ -0,0 +1,7 @@
+build/Debug/GNU-Linux/main.o: main.c fuse.h fuse_common.h fuse_opt.h
+
+fuse.h:
+
+fuse_common.h:
+
+fuse_opt.h:
---
+++ main.c
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/* 
+ * File:   main.c
+ * Author: muhammadqasimhunain
+ *
+ * Created on April 18, 2017, 9:30 PM
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include "fuse.h"
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+
+    printf("Hello Fuse\n");
+    return (EXIT_SUCCESS);
+}
+
